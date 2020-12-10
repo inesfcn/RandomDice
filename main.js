203fa85690d38fc385601f4076f066d231dc95ae
Ines <inesfcnogueira@gmail.com>
2020-12-10 12:12:12 +0100

    fixed

--- main.js
+++ main.js
@@ -1,22 +1,49 @@
-let dice = document.getElementsByClassName("dice");
+let dice = document.querySelector(".dice");
+const allDots = Array.from(document.querySelectorAll(".dice div"));
 
-function addDot(){
-    let div = document.createElement("div");
-    div.className = "dot"
-    dice[0].appendChild(div)
+/* function addDot() {
+  let div = document.createElement("div");
+  div.className = "dot";
+  dice.appendChild(div);
+} */
+
+function addRandomDot() {
+  allDots.forEach((dot) => dot.classList.remove(...dot.classList));
+  let randomNumber = 1 + Math.floor(Math.random() * 6);
+  if (randomNumber === 1) {
+    allDots[4].classList.add("dot");
+  }
+  if (randomNumber === 2) {
+    allDots[3].classList.add("dot");
+    allDots[5].classList.add("dot");
+  }
+  if (randomNumber === 3) {
+    allDots[3].classList.add("dot");
+    allDots[4].classList.add("dot");
+    allDots[5].classList.add("dot");
+  }
+  if (randomNumber === 4) {
+    allDots[0].classList.add("dot");
+    allDots[2].classList.add("dot");
+    allDots[6].classList.add("dot");
+    allDots[8].classList.add("dot");
+  }
+  if (randomNumber === 5) {
+    allDots[0].classList.add("dot");
+    allDots[2].classList.add("dot");
+    allDots[4].classList.add("dot");
+    allDots[6].classList.add("dot");
+    allDots[8].classList.add("dot");
+  }
+  if (randomNumber === 6) {
+    allDots[0].classList.add("dot");
+    allDots[2].classList.add("dot");
+    allDots[3].classList.add("dot");
+    allDots[5].classList.add("dot");
+    allDots[6].classList.add("dot");
+    allDots[8].classList.add("dot");
+  }
 }
 
-function addRandomDot(){
-    
-    let randomNumber= Math.ceil(Math.random()*6)
-    if(randomNumber>1){
-    for(i=0;i<randomNumber-1;i++){
-        addDot()
-    }
-    dice[0].removeEventListener("click", addRandomDot);
-}
-}
-
-//  addRandomDot()
-
-dice[0].addEventListener("click", addRandomDot);
+//dice.addEventListener("click", removeDot);
+dice.addEventListener("click", addRandomDot);
